85d9e5dcb683e6001fe5228f0d3d440001218888
Chris Brenton <chrisbrenton@gmail.com>
2020-03-22 23:38:16 -0700

    Create a builder for creating the deferred rendering RenderPasses. Move SimpleScene into the newly created builders dir

---
+++ src/builders/deferred-pass-builder.js
@@ -0,0 +1,97 @@
+'use strict';
+
+import {m4, v3} from 'twgl.js';
+import {BufferTarget} from 'render/buffer-target.js';
+import {ScreenTarget} from 'render/screen-target.js';
+import {ShaderManager} from 'managers/shader-manager.js';
+import {RenderPass} from 'render/render-pass.js';
+
+export function createDeferredPasses(gl, sceneManager) {
+  return [
+    createGBufferPass(gl, sceneManager),
+    createLBufferPass(gl, sceneManager),
+    createOverlayPass(gl, sceneManager),
+  ];
+}
+
+/**
+ * Create a render pass to hold gbuffer data.
+ * @param {WebGL2RenderingContext} gl
+ * @param {SceneManager} sceneManager
+ * @return {RenderPass}
+ */
+function createGBufferPass(gl, sceneManager) {
+  const attachments = ['albedo', 'normal', 'shininess'];
+  const gBufferTarget = new BufferTarget(
+      gl, gl.canvas.clientWidth, gl.canvas.clientHeight, attachments);
+
+  const setUp = function(cb_gl) {
+    cb_gl.clearColor(0.58, 0.78, 0.85, 1);
+    cb_gl.clear(cb_gl.COLOR_BUFFER_BIT | cb_gl.DEPTH_BUFFER_BIT);
+    cb_gl.enable(cb_gl.DEPTH_TEST);
+  };
+
+  const tearDown = function() {};
+
+  const gBufferPass = new RenderPass(
+      gl, gBufferTarget, ShaderManager.shader('gBuffer'),
+      sceneManager.geometry.main, sceneManager.cameras[0], setUp, tearDown, {});
+  return gBufferPass;
+}
+
+/**
+ * Create a render pass to hold lighting results.
+ * @param {WebGL2RenderingContext} gl
+ * @param {SceneManager} sceneManager
+ * @return {RenderPass}
+ */
+function createLBufferPass(gl, sceneManager) {
+  const attachments = ['result'];
+  const lBufferTarget = new BufferTarget(
+      gl, gl.canvas.clientWidth, gl.canvas.clientHeight, attachments);
+
+  const setUp = function(cb_gl) {
+    cb_gl.clearColor(0.58, 0.78, 0.85, 1);
+    cb_gl.clear(cb_gl.COLOR_BUFFER_BIT | cb_gl.DEPTH_BUFFER_BIT);
+    cb_gl.enable(cb_gl.DEPTH_TEST);
+  };
+
+  const tearDown = function() {};
+
+  const lBufferPass = new RenderPass(
+      gl, lBufferTarget, ShaderManager.shader('lBuffer'),
+      sceneManager.geometry.lights, sceneManager.cameras[0], setUp, tearDown,
+      {});
+  return lBufferPass;
+}
+
+/**
+ * Create a render pass to display overlay geometry.
+ * @param {WebGL2RenderingContext} gl
+ * @param {SceneManager} sceneManager
+ * @return {RenderPass}
+ */
+function createOverlayPass(gl, sceneManager) {
+  const overlayTarget = new ScreenTarget(gl);
+
+  const overlayCamera = {
+    viewMatrix: m4.identity(),
+    projMatrix: m4.identity(),
+    position: v3.create(0, 0, 0),
+  };
+
+  const setUp = function(cb_gl) {
+    cb_gl.disable(cb_gl.DEPTH_TEST);
+    cb_gl.clearColor(0.58, 0.78, 0.85, 1);
+    cb_gl.clear(cb_gl.COLOR_BUFFER_BIT | cb_gl.DEPTH_BUFFER_BIT);
+  };
+
+  const tearDown = function(cb_gl) {
+    cb_gl.enable(cb_gl.DEPTH_TEST);
+  };
+
+  const overlayPass = new RenderPass(
+      gl, overlayTarget, ShaderManager.shader('flatTexture'),
+      sceneManager.geometry.overlay, overlayCamera, setUp, tearDown, {});
+  return overlayPass;
+}
--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
 'use strict';
 
-import {m4, v3} from 'twgl.js';
 import * as util from 'util/scene-helpers.js';
 import {logFrame} from 'util/fps-counter.js';
-import {createSimpleScene} from 'scene/simple-scene.js';
+import {createSimpleScene} from 'builders/simple-scene-builder.js';
+import {createDeferredPasses} from 'builders/deferred-pass-builder.js';
 import {TextureManager} from 'managers/texture-manager.js';
-import {ShaderManager} from 'managers/shader-manager.js';
 import {GLContextManager} from 'managers/gl-context-manager.js';
-import {RenderPass} from 'render/render-pass.js';
-import {BufferTarget} from 'render/buffer-target.js';
-import {ScreenTarget} from 'render/screen-target.js';
 
 window.onload = function() {
   const gl = GLContextManager.gl;
@@ -21,96 +17,17 @@ window.onload = function() {
 
 /**
  * Create a scene graph holding all objects in the scene.
- * @return {SimpleScene}
+ * @return {Object}
  */
 function createSceneInfo(gl) {
   const result = {};
   result.graph = createSimpleScene(gl, TextureManager.textures);
-  result.renderPasses = [
-    createGBufferPass(gl, result.graph),
-    createLBufferPass(gl, result.graph),
-    createOverlayPass(gl, result.graph),
-  ];
+  result.renderPasses = createDeferredPasses(gl, result.graph);
   result.graph.addGBufferToHUD(result.renderPasses[0].renderTarget)
+  result.graph.addGBufferToHUD(result.renderPasses[1].renderTarget)
   result.graph.addScreenAlignedQuad(
       result.renderPasses[1].renderTarget.colorAttachments.result);
   return result;
-}
-
-/**
- * Create and initialize the gbuffer rendering pass.
- * @param {WebGL2RenderingContext} gl
- * @param {SceneManager} sceneManager
- * @return {RenderPass}
- */
-function createGBufferPass(gl, sceneManager) {
-  const attachments = ['albedo', 'normal', 'shininess'];
-  const gBufferTarget = new BufferTarget(
-      gl, gl.canvas.clientWidth, gl.canvas.clientHeight, attachments);
-
-  const setUp = function(cb_gl) {
-    cb_gl.clearColor(0.58, 0.78, 0.85, 1);
-    cb_gl.clear(cb_gl.COLOR_BUFFER_BIT | cb_gl.DEPTH_BUFFER_BIT);
-    cb_gl.enable(cb_gl.DEPTH_TEST);
-  };
-
-  const tearDown = function() {};
-
-  const gBufferPass = new RenderPass(
-      gl, gBufferTarget, ShaderManager.shader('gBuffer'),
-      sceneManager.geometry.main, sceneManager.cameras[0], setUp, tearDown, {});
-  return gBufferPass;
-}
-
-/**
- * Create and initialize a gbuffer for lighting results.
- * // @TODO update docstring
- * @param {WebGL2RenderingContext} gl
- * @return {GBuffer}
- */
-function createLBufferPass(gl, sceneManager) {
-  const attachments = ['result'];
-  const lBufferTarget = new BufferTarget(
-      gl, gl.canvas.clientWidth, gl.canvas.clientHeight, attachments);
-
-  const setUp = function(cb_gl) {
-    cb_gl.clearColor(0.58, 0.78, 0.85, 1);
-    cb_gl.clear(cb_gl.COLOR_BUFFER_BIT | cb_gl.DEPTH_BUFFER_BIT);
-    cb_gl.enable(cb_gl.DEPTH_TEST);
-  };
-
-  const tearDown = function() {};
-
-  const lBufferPass = new RenderPass(
-      gl, lBufferTarget, ShaderManager.shader('lBuffer'),
-      sceneManager.geometry.lights, sceneManager.cameras[0], setUp, tearDown,
-      {});
-  return lBufferPass;
-}
-
-function createOverlayPass(gl, sceneManager) {
-  const overlayTarget = new ScreenTarget(gl);
-
-  const overlayCamera = {
-    viewMatrix: m4.identity(),
-    projMatrix: m4.identity(),
-    position: v3.create(0, 0, 0),
-  };
-
-  const setUp = function(cb_gl) {
-    cb_gl.disable(cb_gl.DEPTH_TEST);
-    cb_gl.clearColor(0.58, 0.78, 0.85, 1);
-    cb_gl.clear(cb_gl.COLOR_BUFFER_BIT | cb_gl.DEPTH_BUFFER_BIT);
-  };
-
-  const tearDown = function(cb_gl) {
-    cb_gl.enable(cb_gl.DEPTH_TEST);
-  };
-
-  const overlayPass = new RenderPass(
-      gl, overlayTarget, ShaderManager.shader('flatTexture'),
-      sceneManager.geometry.overlay, overlayCamera, setUp, tearDown, {});
-  return overlayPass;
 }
 
 /**
