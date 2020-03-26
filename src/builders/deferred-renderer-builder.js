ed25b1b2b7fb04b151370e52158abbad0f815111
Chris Brenton <chrisbrenton@gmail.com>
2020-03-25 18:07:29 -0700

    Use blend with blendFunc (gl.ONE, gl.ONE) for l-buffer pass, and set clearColor to black

--- src/builders/deferred-renderer-builder.js
+++ src/builders/deferred-renderer-builder.js
@@ -84,14 +84,17 @@ function createLBufferPass(gl, sceneManager, gbuffer) {
     gl.enable(gl.CULL_FACE);
     gl.cullFace(gl.FRONT);
     gl.clearColor(0, 0, 0, 1);
-    gl.clearColor(0.58, 0.78, 0.85, 1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.enable(gl.DEPTH_TEST);
+    gl.disable(gl.DEPTH_TEST);
+    gl.enable(gl.BLEND);
+    gl.blendEquation(gl.FUNC_ADD);
+    gl.blendFunc(gl.ONE, gl.ONE);
   };
 
   const tearDown = function() {
     gl.disable(gl.CULL_FACE);
     gl.cullFace(gl.BACK);
+    gl.disable(gl.BLEND);
   };
 
   const uniforms = {
